df1e6b910d1dbc4d97d84127952230a9a3d84c44
Ivan Egorov <vany.egorov@gmail.com>
2022-03-27 23:20:47 +0300

    make FnT (a sort of log Handler of log function) implements a Logger interface.

--- type.go
+++ type.go
@@ -1,6 +1,11 @@
 package log
 
 type FnT func(Level, string, ...interface{})
+
+func (f FnT) Log(l Level, msg string, a ...interface{}) {
+	f(l, msg, a...)
+}
+
 type FnTShrt func(Level, string)
 
 type Logger interface {
