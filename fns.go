7ff10233309573cece0b586ad750917fc1807150
Mikhail <rogushkov@gmail.com>
2022-12-16 23:37:11 +0300

    add supporting zap logger

--- fns.go
+++ fns.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 
 	"github.com/cihub/seelog"
+	"go.uber.org/multierr"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	_multipleErrMsg = "Multiple errors without a key."
 )
 
 var (
@@ -25,6 +32,11 @@ var (
 	toStderr  = func(msg string, a ...interface{}) { toWriter(os.Stderr, msg, a...) }
 	toStdout  = func(msg string, a ...interface{}) { toWriter(os.Stdout, msg, a...) }
 )
+
+type invalidPair struct {
+	position   int
+	key, value interface{}
+}
 
 func LogFn(any logger, l Level) func(string, ...interface{}) {
 	switch lgr := any.(type) {
@@ -48,24 +60,92 @@ func LogFn(any logger, l Level) func(string, ...interface{}) {
 
 			lgr.Flush()
 		}
-		// case *zap.Logger:
-		// 	return func(msg string) {
-		// 		switch l {
-		// 		case Quiet:
-		// 		case Trace, Debug:
-		// 			lgr.Debug(msg)
-		// 		case Info:
-		// 			lgr.Info(msg)
-		// 		case Warn:
-		// 			lgr.Warn(msg)
-		// 		case Error:
-		// 			lgr.Warn(msg) // TODO: research for non-panicable Error for zap
-		// 		case Critical:
-		// 			lgr.Fatal(msg)
-		// 		}
+	case *zap.SugaredLogger:
+		return func(msg string, a ...interface{}) {
+			switch l {
+			case Quiet:
+			case Trace, Debug:
+				lgr.Debugw(msg, a...)
+			case Info:
+				lgr.Infof(msg, a...)
+			case Warn:
+				lgr.Warnf(msg, a...)
+			case Error:
+				lgr.Errorf(msg, a...)
+			case Critical:
+				lgr.Fatalf(msg, a...)
+			}
+		}
+	case *zap.Logger:
+		return func(msg string, a ...interface{}) {
+			var param []zap.Field
+			if len(a) != 0 {
+				param = make([]zap.Field, 0, len(a))
+				var (
+					invalid   invalidPairs
+					seenError bool
+				)
+				for i := 0; i < len(a); {
+					// This is a strongly-typed field. Consume it and move on.
+					if f, ok := a[i].(zap.Field); ok {
+						param = append(param, f)
+						i++
+						continue
+					}
 
-		// 		lgr.Sync()
-		// 	}
+					// If it is an error, consume it and move on.
+					if err, ok := a[i].(error); ok {
+						if !seenError {
+							seenError = true
+							param = append(param, zap.Error(err))
+						} else {
+							lgr.With().Error(_multipleErrMsg, zap.Error(err))
+						}
+						i++
+						continue
+					}
+
+					// Make sure this element isn't a dangling key.
+					if i == len(a)-1 {
+						param = append(param, zap.Any("ignored", a[i]))
+						break
+					}
+
+					// Consume this value and the next, treating them as a key-value pair. If the
+					// key isn't a string, add this pair to the slice of invalid pairs.
+					key, val := a[i], a[i+1]
+					if keyStr, ok := key.(string); !ok {
+						// Subsequent errors are likely, so allocate once up front.
+						if cap(invalid) == 0 {
+							invalid = make(invalidPairs, 0, len(a)/2)
+						}
+						invalid = append(invalid, invalidPair{i, key, val})
+					} else {
+						param = append(param, zap.Any(keyStr, val))
+					}
+					i += 2
+				}
+
+				// If we encountered any invalid key-value pairs, log an error.
+				if len(invalid) > 0 {
+					param = append(param, zap.Array("invalid", invalid))
+				}
+			}
+			switch l {
+			case Quiet:
+			case Trace, Debug:
+				lgr.Debug(msg, param...)
+			case Info:
+				lgr.Info(msg, param...)
+			case Warn:
+				lgr.Warn(msg, param...)
+			case Error:
+				lgr.Error(msg, param...)
+			case Critical:
+				lgr.Fatal(msg, param...)
+			}
+
+		}
 	}
 
 	switch l {
@@ -88,3 +168,20 @@ func LogStd(l Level, msg string, a ...interface{}) { LogFn(nil, l)(msg, a...) }
 func Logf(any logger, l Level, msg string, a ...interface{}) { LogFn(any, l)(msg, a...) }
 
 func LogfStd(l Level, msg string, a ...interface{}) { LogStd(l, msg, a...) }
+
+type invalidPairs []invalidPair
+
+func (ps invalidPairs) MarshalLogArray(enc zapcore.ArrayEncoder) error {
+	var err error
+	for i := range ps {
+		err = multierr.Append(err, enc.AppendObject(ps[i]))
+	}
+	return err
+}
+
+func (p invalidPair) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddInt64("position", int64(p.position))
+	zap.Any("key", p.key).AddTo(enc)
+	zap.Any("value", p.value).AddTo(enc)
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/go-x-pkg/log
 go 1.13
 
 require (
-  github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
+	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
+	go.uber.org/zap v1.24.0 // indirect
 )
